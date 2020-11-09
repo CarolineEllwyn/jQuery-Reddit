3e984c6283d760d18f915553aa6500e6386168e9
cellwy <caroline.ellwyn@gmail.com>
2020-11-09 19:06:21 +0100

    feat: jQuary animation and style with boostrap

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    const $content = $('#restultDiv'); 
+    const $content = $('body');
 
     $.ajax({
-        method:"GET",
-        url: 'https://www.reddit.com/r/funny/top.json?top=15',   //url to top 15 posts
-        success: function(data) {                               //if successful, the method retrieves a permalink and the title from API and turns it into a link named by the title
-            $(data.data.children).each(function(index, item){
+        method: "GET",
+        url: 'https://www.reddit.com/r/funny/top.json?limit=15',   //url to 15 posts
+        success: function (data) {                               //if successful, the method retrieves a permalink, a picture and the title from API and turns it into a link named by the title
+            $(data.data.children).each(function (index, item) {
                 const result = `
-                <div class="post-body">
-                    <div class="post-title"><a href="https://www.reddit.com${item.data.permalink}">${item.data.title}</a></div>
+                <div class="container">
+                <div class="row">
+                    <div class="col-12 pb-4 d-flex justify-content-center">
+                          <div class="card mb-3">
+                          <img class="card-img-top" src="${item.data.url}" alt="image.jpg" style="width: 800px; height:500px;">
+                                        <div class="card-body">
+                                        <h5 class="card-title"><a id="text-animate" href="https://www.reddit.com${item.data.permalink}">${item.data.title}</a></h5>
+                                         </div>
+                                        </div>
+                    </div>
                 </div>
+                </div>
+
             `
-            $content.append(result); // add result from the AJAX into the content variable.
-            })           
+                $content.append(result); // add result from the AJAX into the content variable.
+                $(".card-title").click(function () { //an animation thats make the title on every card fade away after click.
+                    $("body").animate({
+                        opacity: 0.5,
+                    }, 1000, function () {
+                    })
+                })
+            })
         },
-        error: function(error) {
+        error: function (error) {
             console.log('error: ', error);
         }
-        
     })
+
 })
+
+
+
+
 
 
 
